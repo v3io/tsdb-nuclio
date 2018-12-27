fd777201e6f2d5180ccd086af944fcbf09d6ed65
Golan <gshatz@gmail.com>
2018-12-27 14:43:38 +0200

    create container instead of data-bindings

--- functions/ingest/ingest.go
+++ functions/ingest/ingest.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/v3io/tsdb-nuclio/functions/ingest/format"
 	"os"
+	"strconv"
 	"sync"
 
 	"github.com/nuclio/nuclio-sdk-go"
@@ -75,8 +76,31 @@ func createTSDBAppender(context *nuclio.Context, path string) (tsdb.Appender, er
 			return nil, err
 		}
 
+		//create the http container
+		v3ioUrl := os.Getenv("V3IO_URL")
+		numWorkers, err := strconv.Atoi(os.Getenv("V3IO_NUM_WORKERS"))
+		if err != nil {
+			return nil, err
+		}
+		ctx, err := v3io.NewContext(context.Logger, v3ioUrl, numWorkers)
+		if err != nil {
+			return nil, err
+		}
+		session, err := ctx.NewSession(os.Getenv("V3IO_USERNAME"), os.Getenv("V3IO_PASSWORD"), "")
+		if err != nil {
+			return nil, err
+		}
+		containerName := os.Getenv("V3IO_CONTAINER")
+		if containerName == "" {
+			containerName = "bigdata"
+		}
+		container, err := session.NewContainer(containerName)
+		if err != nil {
+			return nil, err
+		}
+
 		// create adapter once for all contexts
-		adapter, err = tsdb.NewV3ioAdapter(v3ioConfig, context.DataBinding["db0"].(*v3io.Container), context.Logger)
+		adapter, err = tsdb.NewV3ioAdapter(v3ioConfig, container, context.Logger)
 		if err != nil {
 			return nil, err
 		}
