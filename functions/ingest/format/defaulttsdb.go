45337c817e1611dc4612a1c36299555b41cb6509
Tal Neiman <Taln@iguazio.com>
2019-02-19 11:43:46 +0200

    support ingesting multiple metrics and labelsets

--- functions/ingest/format/defaulttsdb.go
+++ functions/ingest/format/defaulttsdb.go
@@ -53,39 +53,41 @@ type request struct {
 type defaultTsdb struct{}
 
 func (Ingester defaultTsdb) Ingest(tsdbAppender tsdb.Appender, event nuclio.Event) error {
-	var request request
+	var requests []request
 
 	// parse body
-	if err := json.Unmarshal(event.GetBody(), &request); err != nil {
+	if err := json.Unmarshal(event.GetBody(), &requests); err != nil {
 		return nuclio.WrapErrBadRequest(err)
 	}
 
-	// convert the map[string]string -> []Labels
-	labels := getLabelsFromRequest(request.Metric, request.Labels)
+	for _, request := range requests {
+		// convert the map[string]string -> []Labels
+		labels := getLabelsFromRequest(request.Metric, request.Labels)
 
-	var ref uint64
-	// iterate over request samples
-	for _, sample := range request.Samples {
+		var ref uint64
+		// iterate over request samples
+		for _, sample := range request.Samples {
 
-		// if time is not specified assume "now"
-		if sample.Time == "" {
-			sample.Time = "now"
-		}
+			// if time is not specified assume "now"
+			if sample.Time == "" {
+				sample.Time = "now"
+			}
 
-		// convert time string to time int, string can be: now, now-2h, int (unix milisec time), or RFC3339 date string
-		sampleTime, err := utils.Str2unixTime(sample.Time)
-		if err != nil {
-			return errors.Wrap(err, "Failed to parse time: "+sample.Time)
-		}
+			// convert time string to time int, string can be: now, now-2h, int (unix milisec time), or RFC3339 date string
+			sampleTime, err := utils.Str2unixTime(sample.Time)
+			if err != nil {
+				return errors.Wrap(err, "Failed to parse time: "+sample.Time)
+			}
 
-		// append sample to metric
-		if ref == 0 {
-			ref, err = tsdbAppender.Add(labels, sampleTime, sample.Value.N)
-		} else {
-			err = tsdbAppender.AddFast(labels, ref, sampleTime, sample.Value.N)
-		}
-		if err != nil {
-			return errors.Wrap(err, "Failed to add sample")
+			// append sample to metric
+			if ref == 0 {
+				ref, err = tsdbAppender.Add(labels, sampleTime, sample.Value.N)
+			} else {
+				err = tsdbAppender.AddFast(labels, ref, sampleTime, sample.Value.N)
+			}
+			if err != nil {
+				return errors.Wrap(err, "Failed to add sample")
+			}
 		}
 	}
 	return nil
