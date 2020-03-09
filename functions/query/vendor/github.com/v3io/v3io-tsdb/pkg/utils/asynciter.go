d13761539ef0b4ea64efd647d382df8816f14584
Gal Topper <galt@iguazio.com>
2020-03-09 11:54:03 +0200

    Revert "Unwrap more errors."
    This reverts commit 0adcccf5f19256f5854d7fe29dbb23e3ec466346.

--- functions/query/vendor/github.com/v3io/v3io-tsdb/pkg/utils/asynciter.go
+++ functions/query/vendor/github.com/v3io/v3io-tsdb/pkg/utils/asynciter.go
@@ -21,10 +21,12 @@ such restriction.
 package utils
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/nuclio/logger"
+	"github.com/pkg/errors"
 	"github.com/v3io/v3io-go/pkg/dataplane"
 	"github.com/v3io/v3io-go/pkg/errors"
 	"github.com/v3io/v3io-tsdb/pkg/config"
@@ -207,7 +209,7 @@ func (ic *AsyncItemsCursor) sendNextGetItemsOld(resp *v3io.Response) error {
 
 		_, err := ic.container.GetItems(input, input, ic.responseChan)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "Failed to request next items")
 		}
 
 	} else {
@@ -254,7 +256,7 @@ func (ic *AsyncItemsCursor) sendNextGetItemsNew(resp *v3io.Response) error {
 
 		_, err := ic.container.GetItems(input, input, ic.responseChan)
 		if err != nil {
-			return err
+			return errors.Wrap(err, fmt.Sprintf("Failed to request next items for input=%v", input))
 		}
 
 	} else {
